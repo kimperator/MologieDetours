389c3ad2a72c12f7c4f341688f61a8a2d7c9a012
kImperator <kajot@gmx.net>
2011-11-06 19:55:16 +0100

    Initial import from http://code.google.com/p/x360ce/source/browse/trunk/x360ce/MologieDetours?spec=svn437&r=437
    due to removed boost reference
    
    added cmake files

---
+++ examples/02_createfilew.cpp
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include "../detours.h"
+
+typedef HANDLE ( WINAPI* tCreateFileW )(LPCWSTR lpFileName,DWORD dwDesiredAccess,DWORD dwShareMode,LPSECURITY_ATTRIBUTES lpSecurityAttributes,DWORD dwCreationDisposition,DWORD dwFlagsAndAttributes,HANDLE hTemplateFile);
+HANDLE WINAPI hook_CreateFileW(LPCWSTR lpFileName,DWORD dwDesiredAccess,DWORD dwShareMode,LPSECURITY_ATTRIBUTES lpSecurityAttributes,DWORD dwCreationDisposition,DWORD dwFlagsAndAttributes,HANDLE hTemplateFile);
+MologieDetours::Detour<tCreateFileW>* detour_CreateFileW = NULL;
+
+HANDLE WINAPI hook_CreateFileW(LPCWSTR lpFileName,DWORD dwDesiredAccess,DWORD dwShareMode,LPSECURITY_ATTRIBUTES lpSecurityAttributes,DWORD dwCreationDisposition,DWORD dwFlagsAndAttributes,HANDLE hTemplateFile) {
+	MessageBoxW(NULL, lpFileName, L"opened", MB_OK);
+	return detour_CreateFileW->GetOriginalFunction()(lpFileName,dwDesiredAccess,dwShareMode,lpSecurityAttributes,dwCreationDisposition,dwFlagsAndAttributes,hTemplateFile);
+}
+
+extern "C" BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
+{
+	switch(fdwReason)
+	{
+		case DLL_PROCESS_ATTACH:
+			try
+			{
+				HMODULE Kernel32 = GetModuleHandle("Kernel32.dll");	
+				detour_CreateFileW = new MologieDetours::Detour<tCreateFileW>(Kernel32, "CreateFileW", hook_CreateFileW);
+			}
+			catch(MologieDetours::DetourException &e)
+			{
+				// Something went wrong
+				
+				return FALSE;
+			}
+			break;
+		case DLL_PROCESS_DETACH:
+			delete detour_CreateFileW;
+			break;
+	}
+	return TRUE;
+}
---
+++ examples/03_messageboxw.cpp
@@ -0,0 +1,34 @@
+#include <windows.h>
+#include "../detours.h"
+
+typedef int (WINAPI *tMessageBoxW)(HWND, LPCWSTR, LPCWSTR, UINT);
+MologieDetours::Detour<tMessageBoxW>* detour_MessageBoxW = NULL;
+
+int WINAPI hook_MessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption,
+		UINT uType) {
+	return detour_MessageBoxW->GetOriginalFunction()(hWnd, L"hook_MessageBoxW",
+			lpCaption, uType);
+}
+
+extern "C" BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
+{
+	switch(fdwReason)
+	{
+		case DLL_PROCESS_ATTACH:
+			try
+			{
+				HMODULE User32 = GetModuleHandle("User32.dll");	
+				detour_MessageBoxW = new MologieDetours::Detour<tMessageBoxW>(User32, "MessageBoxW", hook_MessageBoxW);
+			}
+			catch(MologieDetours::DetourException &e)
+			{
+				// Something went wrong
+				return FALSE;
+			}
+			break;
+		case DLL_PROCESS_DETACH:
+			delete detour_MessageBoxW;
+			break;
+	}
+	return TRUE;
+}
